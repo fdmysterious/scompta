f71017beca90b5c489fbfac8fda19d21fd4cffc7
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-10-30 16:36:09 +0100

    Use new classes

--- test_filter.py
+++ test_filter.py
@@ -1,20 +1,21 @@
 from scompta import transactions, accounts
 from pathlib import Path
 
-import scompta.db.accounts
-import scompta.db.transactions
+import scompta.db.accounts     as accounts_db
+import scompta.db.transactions as transactions_db
 
-df_accounts     = scompta.db.accounts.load_from_dir("./accounts")
-#df_transactions = transactions.load("transactions.csv")
-df_transactions = scompta.db.transactions.load("transactions.csv")
+from   scompta.views import transactions as transactions_views
+
+df_accounts     = accounts_db.load_from_dir("./accounts")
+df_transactions = transactions_db.load("transactions.csv")
 
 print(df_accounts)
 print(df_transactions)
 
-print(transactions.undefined_accounts(df_transactions, df_accounts))
+print(transactions_views.undefined_accounts(df_transactions, df_accounts))
 
-df_input        = transactions.input (df_transactions, df_accounts, "actifs/florian/ca_courant")
-df_output       = transactions.output(df_transactions, df_accounts, "actifs/florian/ca_courant")
+df_input        = transactions_views.input (df_transactions, df_accounts, "actifs/florian/ca_courant")
+df_output       = transactions_views.output(df_transactions, df_accounts, "actifs/florian/ca_courant")
 
 v_input         = df_input["amount"].sum()
 v_output        = df_output["amount"].sum()
